fe69e5a90c05a340ae39269970aca9ffb33cb118
Sze Tyng Lee <lee.szetyng@gmail.com>
2019-02-10 22:57:16 +0000

    stuff

--- main.fsx
+++ main.fsx
@@ -2,27 +2,30 @@ type Agent<'T> = MailboxProcessor<'T>
 type Message = 
     | Apply of Agent<Message>*Agent<Message>*AsyncReplyChannel<bool>
     | Appropriate of int*Agent<Message>
+    // TODO
+    // Exclude
+    // Demand
+    // Allocate
+    // Report
 
 type Holon(name) = 
     // member institution properties
     let mutable memberOf:Holon list = []
+    let mutable offenceLevel = 0
     let mutable sanctionLevel = 0
-
+    // TODO -> set characteristics of the agent
+    // eg: propensity to misappropriate
+    // TOTHINK -> difference between pow, per and obl?
+    
     // supra-institution properties
     let mutable members:Holon list = []
     let mutable gatekeeper:Holon list = []
     let mutable head:Holon list = []
-    let mutable sanctionLimit = 0
+    let mutable sanctionLimit = 2
     let mutable membershipLimit = 0
 
-    let membershipCriteria inst applicant = true
-
-    let checkApplication (inst:Agent<Message>) (applicant:Agent<Message>) (replyCh:AsyncReplyChannel<bool>) = 
-        if membershipCriteria inst applicant then
-            replyCh.Reply(true)
-        else 
-            replyCh.Reply(false)
     let addMember newbie = members <- (List.append members [newbie])      
+    let includeMember mem (replyCh:AsyncReplyChannel<bool>) = replyCh.Reply(true)
 
     let agent = Agent.Start(fun agent ->
         let rec loop () = async {
@@ -33,14 +36,16 @@ type Holon(name) =
                 return! loop()
             | Apply(inst,friend,replyCh) ->
                 //printfn "Received %A" fullMsg
+                // I am the gatekeeper
                 printfn "%A received Apply" name 
-                checkApplication inst friend replyCh
+                includeMember friend replyCh
                 return! loop()                        
         }
         loop()
     )
 
-    // Properties
+    // TODO -> make properties set and get instead
+    // Properties, so that they can be read
     member this.Self = agent
     member this.Name = name
     member this.MemberOf = memberOf
@@ -50,7 +55,6 @@ type Holon(name) =
     member this.SanctionLevel = sanctionLevel
     member this.SanctionLimit = sanctionLimit
     member this.MembershipLimit = membershipLimit
-
 
     // Setting properties
     member this.JoinHolon h = memberOf <- (List.append memberOf [h])
@@ -64,14 +68,14 @@ type Holon(name) =
     member this.SetInstSize n = membershipLimit <- n
     member this.SendApplication (inst:Agent<Message>) (hplusGatekeeper:Holon) = 
         let reply = 
-            hplusGatekeeper.Self.PostAndReply(fun replyChannel -> (Apply(inst,agent,replyChannel)))
+            hplusGatekeeper.Self.PostAndReply(fun replyChannel -> (Apply(inst, agent, replyChannel)))
         reply        
 
 /// Applicant applies to inst  
 /// inst's gatekeeper checks the application
 // maybe include criteria as a function input
 let applyToInstitution (applicant:Holon) (inst:Holon) = 
-    let crit = [applicant.SanctionLevel > inst.SanctionLimit ; inst.MembershipLimit > inst.MemberOf.Length]
+    let crit = [applicant.SanctionLevel < inst.SanctionLimit ; inst.MembershipLimit > inst.MemberOf.Length]
     let rec checking (supraH:Holon) (h:Holon) (lst:bool list) = 
         match lst with
         | [] -> true
@@ -100,7 +104,7 @@ let applyToInstitution (applicant:Holon) (inst:Holon) =
 
 
 let createParks(parksLst) =
-    let parks = new Holon("parks",100)
+    let parks = Holon("parks")
 
     parks.SetMembers parksLst
     List.map (fun (h:Holon) -> h.JoinHolon parks) parksLst |> ignore
@@ -108,26 +112,26 @@ let createParks(parksLst) =
     parks.SetGatekeeper (List.head parksLst)
 
     // Principle 1: Membership criteria
-    parks.SetSanctionLimit 20
+    parks.SetSanctionLimit 2
     parks.SetInstSize 6
     
     parks
 
-let leslie = new Holon("leslie",100)
-let ron = new Holon("ron",100)
-let tom = new Holon("tom", 70)
-let april = new Holon("april", 70)
+let leslie = Holon("leslie")
+let ron = Holon("ron")
+let tom = Holon("tom")
+let april = Holon("april")
 let parksLst = [leslie; ron; tom; april]
 let parks = createParks parksLst
 
 
-let ben = new Holon("ben",100)
+let ben = Holon("ben")
 parks.Members
 applyToInstitution ben parks
 
 parks.Members.Length
 ben.MemberOf
 
-let mark = new Holon("mark",10)
+let mark = Holon("mark")
 applyToInstitution mark parks
 
